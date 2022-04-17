29777607a2a1c6122f8f8bf52f6882047525b031
Lee Coronilia <shadowwolf246815@gmail.com>
2022-04-17 00:14:56 -0700

    preloaded sparkle and replaced explosion

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,7 +6,7 @@ class Menu extends Phaser.Scene {
     preload() {
         // load audio
         this.load.audio('sfx_select', './assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sparkle', './assets/sparkle.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
 
         //load menu images
